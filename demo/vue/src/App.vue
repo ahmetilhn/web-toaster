a82a4132b85ffe05dd20c9eb31a10c2bc7c434dd
ahmetilhn <ahmetilhan.dev@gmail.com>
2023-09-14 01:54:21 +0300

    notification and container script added

---
+++ demo/vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="showNotifi">Tıkla</button>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import webToaster from "../../../build/index";
+const toaster = new webToaster({});
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    showNotifi() {
+      console.log(toaster.success("test"));
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
